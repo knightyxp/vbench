387acc8efc5af6df76316f8c58f750d47aecac16
Xiangpeng Yang <knightyxp@gmail.com>
2025-10-05 22:58:38 +1100

    test

--- eval_video.sh
+++ eval_video.sh
@@ -3,9 +3,9 @@
 #     --videos_path /projects/D2DCRC/xiangpeng/Senorita/obj_removal_videos_vie_top1k/0089_source_869aa598c2736f7f4ae90956e5f50ac1_org.mp4 \
 #     --mode=custom_input
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-torchrun --nproc_per_node=2 --standalone evaluate.py \
-    --dimension 'dynamic_degree' \
-    --videos_path /projects/D2DCRC/xiangpeng/Senorita/obj_removal_videos_vie_top1k \
-    --mode=custom_input
+torchrun --nproc_per_node=4 --standalone evaluate.py \
+  --video_json /scratch3/yan204/yxp/Senorita/updated_data_obj_grounding_videos.json \
+  --video_base_dir /scratch3/yan204/yxp/Senorita \
+  --dimension 'dynamic_degree'
